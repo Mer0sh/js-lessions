71d5aea9ba4160dcaf87b024ee414fd97afb5511
mer0sh <galmerkhat007@gmail.com>
2024-08-26 14:59:32 +0500

    Этап2

--- src/string-utils/len.js
+++
@@ -1,8 +0,0 @@
-export function len(text) {
-    if (typeof text !== 'string') {
-        throw Error('argument must be type of string');
-    }
-    let a = 0;
-    while (text[a] !== undefined){a++}   
-    return a;
-}
