ee355286c153d70a2d1503b2caa3f093fd399c1d
mer0sh <galmerkhat007@gmail.com>
2024-08-26 17:04:24 +0500

    реализован repeat с test

--- src/string-utils/len.js
+++ src/string-utils/len.js
@@ -1,2 +1,8 @@
 export function len(text) {
+    if (typeof text !== 'string') {
+        throw Error('argument must be type of string');
+    }
+    let a = 0;
+    while (text[a] !== undefined){a++}   
+    return a;
 }
