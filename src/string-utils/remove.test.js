8ed725e1680c20ead4abf72f5389fb8668dc74d5
Mer0sh <galmerkhat007@gmail.com>
2024-08-29 14:36:36 +0600

    remove js files

--- src/string-utils/remove.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, test, expect } from 'bun:test';
-import { remove } from './remove';
-
-describe('repeatTests', () => {
-  test('', () => {
-    expect(remove('Hello', 'o')).toBe('Hell');
-    expect(remove('world', 'l')).toBe('word');
-  });
-})
